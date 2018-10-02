98febfa47b53aaf5306ccbabe6686c6e81364b17
Cyril MERY <cmery@coffreo.com>
2018-10-02 17:12:41 +0200

    build: update url to use fork instead of mozilla original repo

--- gulpfile.js
+++ gulpfile.js
@@ -63,8 +63,8 @@ var COMMON_WEB_FILES = [
 ];
 var MOZCENTRAL_DIFF_FILE = 'mozcentral.diff';
 
-var REPO = 'git@github.com:mozilla/pdf.js.git';
-var DIST_REPO_URL = 'https://github.com/mozilla/pdfjs-dist';
+var REPO = 'git@github.com:emri99/pdf.js.git';
+var DIST_REPO_URL = 'https://github.com/emri99/pdfjs-dist';
 
 var builder = require('./external/builder/builder.js');
 
